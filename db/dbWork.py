26f9308a77c433c1ad1168957b28c63c27757dd8
reduxvzr <romanch1819@gmail.com>
2024-06-10 11:39:11 +0300

    Revert "made adding data to the database"
    This reverts commit 858bd7dc70bd061fe93edfe7ebc13282eeacc85e.

--- db/dbWork.py
+++ db/dbWork.py
@@ -2,37 +2,40 @@ import psycopg
 
 from configure import host, user, password, db_name
 
-def database(name, rating, comments):
-    connection = None
+def database(): 
+    connection = None  # Initialize connection to None
     try:
+        # Connect to the existing database
         connection = psycopg.connect(
             host=host,
             user=user,
             password=password,
-            dbname=db_name
+            dbname=db_name  # Use dbname instead of db_name for psycopg
         )
         connection.autocommit = True
 
+        # Create cursor 
         with connection.cursor() as cursor:
             cursor.execute("SELECT version()")
             print(f"Server version: {cursor.fetchone()}")
 
+        # create a new Table 
         with connection.cursor() as cursor:
             cursor.execute("""
                 CREATE TABLE IF NOT EXISTS users(
-                    id serial PRIMARY KEY,
-                    fullname varchar(64),
+                    id serial PRIMARY KEY, 
+                    fullname varchar(64), 
                     rating INT CHECK (rating >= 1 AND rating <= 5),
                     suggestion TEXT
-                );
+                    );
             """)
 
-        print("INFO: Таблица была создана успешно!")
+        print("INFO: Таблица была создана успешно!")   
 
-        with connection.cursor() as cursor:
-            cursor.execute("""
-                INSERT INTO users (fullname, rating, suggestion) VALUES (%s, %s, %s);
-            """, (name, rating, comments))
+        with connection.cursor() as cursor: 
+            cursor.execute(""" 
+            INSERT INTO users (fullname, rating, suggestion) VALUES 
+            ('test', 5, 'test');""")
     except Exception as _ex:
         print("INFO: Ошибка при работе с БД PostgreSQL")
         print(_ex)
@@ -43,4 +46,4 @@ def database(name, rating, comments):
 
 
 if __name__ == '__main__':
-    database('test', 5, 'test')
+    database()
